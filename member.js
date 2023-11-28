a256e39c9a8ea617dfc21ebcae08b34f4203a4d7
Aviv Leemann <62147524+avivleemann@users.noreply.github.com>
2023-11-28 10:32:08 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,20 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var projects = document.getElementById("projects");
+  var contact = document.getElementById("contact");
+  var memberButton = document.getElementById("memberButton");
+  var skillsButton = document.getElementById("skillsButton");
+  var projectsButton = document.getElementById("projectsButton");
+  var contactButton = document.getElementById("contactButton");
+
+  member.style.display = "block";
+  skills.style.display = "none";
+  projects.style.display = "none";
+  contact.style.display = "none";
+
+  memberButton.style.backgroundColor = "#4CAF50";
+  skillsButton.style.backgroundColor = "#333";
+  projectsButton.style.backgroundColor = "#333";
+  contactButton.style.backgroundColor = "#333";
+}
